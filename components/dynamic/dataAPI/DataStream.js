2a90f68ba0be32c75224a76444d0dcd6b175edce
reacthcmvn <react.hcmvn@gmail.com>
2022-06-29 18:27:27 +0700

    add 5 boxes

--- components/dynamic/dataAPI/DataStream.js
+++ components/dynamic/dataAPI/DataStream.js
@@ -6,12 +6,18 @@ const DataStream = () => {
       <button className="text-black text-xs bg-yellow-400/60 round-md">
         IMPORT DATA
       </button>
-      <p className="text-white text-xs tracking-widest">>> IMPORT SOCKET</p>
-      <p className="text-white text-xs tracking-widest">>> IMPORT STRUCT</p>
       <p className="text-white text-xs tracking-widest">
-        >> IMPORT INET Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        Perferendis error ducimus necessitatibus cupiditate nisi, tempora ullam
-        exercitationem. Doloremque, nam atque.
+        {" "}
+        &gt;&gt;&gt; IMPORT SOCKET
+      </p>
+      <p className="text-white text-xs tracking-widest">
+        {" "}
+        &gt;&gt;&gt; IMPORT STRUCT
+      </p>
+      <p className="text-white text-xs tracking-widest">
+        &gt;&gt; IMPORT INET Lorem ipsum dolor sit amet, consectetur adipisicing
+        elit. Perferendis error ducimus necessitatibus cupiditate nisi, tempora
+        ullam exercitationem. Doloremque, nam atque.
       </p>
     </div>
   );
